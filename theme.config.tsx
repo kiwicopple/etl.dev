7d78c79b6863422b3de494c4d07b66cfaf677dd8
Copple <10214025+kiwicopple@users.noreply.github.com>
2023-11-21 20:15:55 +0100

    skeleton pages

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>ETL</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/kiwicopple/etl.dev',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Sponsored by Supabase',
   },
 }
 

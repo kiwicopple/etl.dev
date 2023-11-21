5e708bd05e0dde510e90a565555e399e1f0e80b4
Copple <10214025+kiwicopple@users.noreply.github.com>
2023-11-21 20:22:50 +0100

    remove the link to github

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,9 +3,6 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>ETL</span>,
-  project: {
-    link: 'https://github.com/kiwicopple/etl.dev',
-  },
   footer: {
     text: 'Sponsored by Supabase',
   },
